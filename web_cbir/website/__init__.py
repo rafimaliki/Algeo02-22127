a2bcfd46acc7ab33ae6f1138a6b08f41156c57ac
rafi-sama <rafimaliki28@gmail.com>
2023-11-13 23:02:17 +0700

    update front end

--- web_cbir/website/__init__.py
+++ web_cbir/website/__init__.py
@@ -6,12 +6,12 @@ def create_app():
     app.config['SECRET_KEY'] = 'hololive jaya'
 
     from .page_userinput import page_userinput
-    from .page_camera import page_camera
+    from .page_camerainput import page_camerainput
     from .page_webscraping import page_webscraping
     from .page_about import page_about
     
     app.register_blueprint(page_userinput, url_prefix='/')
-    app.register_blueprint(page_camera, url_prefix='/')
+    app.register_blueprint(page_camerainput, url_prefix='/')
     app.register_blueprint(page_webscraping, url_prefix='/')
     app.register_blueprint(page_about, url_prefix='/')
 
--- web_cbir/website/page_camera.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-
-page_camera = Blueprint('page_camera', __name__)
-
-@page_camera.route('/camera', methods=['GET', 'POST'])
-def home():
-
-    return render_template("page_camera.html")
