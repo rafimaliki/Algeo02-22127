cfe294ba049b93fc148406cae116d5707e348002
LastPrism7 <67941358+LastPrism7@users.noreply.github.com>
2023-11-19 17:11:03 +0700

    Update CBIR_Color.py

--- CBIR_Color/CBIR_Color.py
+++ CBIR_Color/CBIR_Color.py
@@ -1,215 +1,133 @@
 from PIL import Image
 from numpy import asarray
-from tqdm import tqdm
-from time import sleep
+from time import process_time 
 import numpy as np
-import matplotlib.pyplot as plt
+import os
 
+t1_start = process_time()  
+counter=0
 
-image1 = Image.open('tes4.jpg')
-image1.show()
-data1 = asarray(image1)
-
-image2= Image.open('tes5.jpg')
-image2.show()
-data2 = asarray(image2)
-
-def Height(Pict):
-    return(Pict.shape[0])
-
-def Width(Pict):
-    return(Pict.shape[1])
-
-def RowSplit3(Pict):
-    H=Height(Pict)
-    p1=H//3
-    p2=2*H//3
-    if H%3==1:
-        p2+=1
-    elif H%3==2:
-        p1+=1
-        p2+=1
-    return p1,p2
-
-def ColSplit3(Pict):
-    W=Width(Pict)
-    p1=W//3
-    p2=2*W//3
-    if W%3==1:
-        p2+=1
-    elif W%3==2:
-        p1+=1
-        p2+=1
-    return p1,p2
-
-def RGBtoHSV(Pict):
+def RGBNorm(Pict):
+    Pict=Pict.astype(np.float32)
     return (np.divide(Pict,255.0))
 
-def Cmax(R,G,B):
-    N=R
-    C=0
-    if R>=G and R>=B:
-        N=R
-        C=0
-    elif G>=R and G>=B:
-        N=G
-        C=1
-    else:
-        N=B
-        C=2
-    return N,C
+def subMatrix(matrix,n):
+    return np.reshape(matrix,(n,matrix.shape[0]//n,n,matrix.shape[1]//n,3),order='C')
 
-def Cmin(R,G,B):
-    N=R
-    C=0
-    if R<=G and R<=B:
-        N=R
-        C=0
-    elif G<=R and G<=B:
-        N=G
-        C=1
-    else:
-        N=B
-        C=2
-    return N,C
-
-def delta(R,G,B):
-    return (Cmax(R,G,B)[0]-Cmin(R,G,B)[0])
-
-def Hue(R,G,B):
-    d=delta(R,G,B)
-    maxType=Cmax(R,G,B)[1]
-    S=0
-    if d>0:
-        if maxType==0:
-            S=(60*(((G-B)/d)%6)%360)
-        elif maxType==1:
-            S=(60*(((B-R)/d)+2)%360)
-        else:
-            S=(60*(((R-G)/d)+4)%360)
-    return S
-
-def Saturation(R,G,B):
-    d=delta(R,G,B)
-    max=Cmax(R,G,B)[0]
-    if max>0:
-        d/=max
-    return d
-
-def Value(R,G,B):
-    return (Cmax(R,G,B)[0])
-
-def VectorNorm(HSVvector):
-    sum=0
-    for i in range(0,14):
-        sum=sum+HSVvector[i]*HSVvector[i]
-    sum=np.sqrt(sum)
-    return sum
-
-def VectorDotMult(HSVvector1,HSVvector2):
-    sum=0
-    for i in range(0,14):
-        sum=sum+HSVvector1[i]*HSVvector2[i]
-    return sum
+def HSV(Pict):
     
-def CosineSim(HSVvector1,HSVvector2):
+    red_channel = Pict[:, :, 0]
+    green_channel = Pict[:, :, 1]
+    blue_channel = Pict[:, :, 2]
+    
+    cmax=np.max(Pict, axis=2)
+    cmin=np.min(Pict, axis=2)
+    delta=np.subtract(cmax,cmin)
+
+    boolmaxR = np.logical_and(delta!=0,cmax==red_channel)
+    boolmaxG = np.logical_and(delta!=0,cmax==green_channel)
+    boolmaxB = np.logical_and(delta!=0,cmax==blue_channel)
+    boolG = np.logical_and(delta==0,delta==0)
+    
+    htemp=np.zeros_like(red_channel,dtype=np.float32)
+    stemp=np.zeros_like(red_channel,dtype=np.float32)
+    vtemp=np.zeros_like(red_channel,dtype=np.float32)
+    
+    red_channel=RGBNorm(red_channel)
+    green_channel=RGBNorm(green_channel)
+    blue_channel=RGBNorm(green_channel)
+
+    cmax=RGBNorm(cmax)
+    delta=RGBNorm(delta)
+
+    htemp[boolG]=0
+    htemp[boolmaxR]=(60*(((green_channel[boolmaxR]-blue_channel[boolmaxR])/delta[boolmaxR])%6))
+    htemp[boolmaxG]=(60*(((blue_channel[boolmaxG]-red_channel[boolmaxG])/delta[boolmaxG])+2))
+    htemp[boolmaxB]=(60*(((red_channel[boolmaxB]-green_channel[boolmaxB])/delta[boolmaxB])+4))
+    
+    stemp[cmax==0]=0
+    stemp[cmax!=0]=delta[cmax!=0]/cmax[cmax!=0]
+
+    vtemp=cmax
+    
+    vector=Quantify(htemp,stemp,vtemp)
+    return vector
+
+def Quantify(hue,sat,val):
+    h=np.copy(hue)
+    s=np.copy(sat)
+    v=np.copy(val)
+
+    h[np.logical_and(h>315, h==0)]=0
+    h[np.logical_and(h>0, h<=25)]=1
+    h[np.logical_and(h>25, h<=40)]=2
+    h[np.logical_and(h>40, h<=120)]=3
+    h[np.logical_and(h>120, h<=190)]=4
+    h[np.logical_and(h>190, h<=270)]=5
+    h[np.logical_and(h>270, h<=295)]=6
+    h[np.logical_and(h>295, h<=315)]=7
+    
+    hueRange = [0,1,2,3,4,5,6,7]
+    huedict = {value: np.count_nonzero(h==value) for value in hueRange}
+    H = np.array(list(huedict.values()),dtype=np.uint32)
+
+    s[np.logical_and(s>0.7, s<=1)]=2
+    s[np.logical_and(s>0.2, s<=0.7)]=1
+    s[np.logical_and(s>=0, s<=0.2)]=0
+    
+    satRange = [0,1,2]
+    satdict = {value: np.count_nonzero(s==value) for value in satRange}
+    S = np.array(list(satdict.values()),dtype=np.uint32)
+
+    v[np.logical_and(v>0.7, v<=1)]=2
+    v[np.logical_and(v>0.2, v<=0.7)]=1
+    v[np.logical_and(v>=0, v<=0.2)]=0
+    
+    valRange = [0,1,2]
+    valdict = {value: np.count_nonzero(v==value) for value in valRange}
+    V = np.array(list(valdict.values()),dtype=np.uint32)
+
+    return np.concatenate((H,S,V))  
+
+def cosineSimAvg(v1,v2):
     hasil=0.0
-    tempDot=VectorDotMult(HSVvector1,HSVvector2)
-    tempNorm1=VectorNorm(HSVvector1)
-    tempNorm2=VectorNorm(HSVvector2)
-    hasil=tempDot/(tempNorm1*tempNorm2)
-    return hasil
+    for i in range(16):
+        vDot=np.dot(v1[i],v2[i])
+        vNorm1=np.linalg.norm(v1[i])
+        vNorm2=np.linalg.norm(v2[i])
+        hasil+=np.divide(vDot,np.multiply(vNorm1,vNorm2))
+    global counter
+    print(counter)
+    counter+=1
+    return round(hasil/16,4)
 
-def Average(Array, N):
-    sum=0.0
-    for i in range (0,N):
-        sum+=Array[i]
-    return sum/float(N)
+def Result(Pict,n):
+    Pict=Image.open(Pict).convert("RGB")
+    Pict=asarray(Pict)
+    subPict=subMatrix(Pict,n)
+    result_array = np.zeros((n*n,14), dtype=np.int64)
+    nth=0
+    for i in range (0,n):
+            for j in range (0,n):
+                result_array[nth]=HSV(subPict[i,:,j,:,:])
+                nth+=1
+    return result_array
 
-def Quantification(HSVvector,Pixel):
-    h=Hue(Pixel[0],Pixel[1],Pixel[2])
-    if (h>=316 and h<=360)or h==0:
-        HSVvector[0]+=1
-    if h>=1 and h<=25:
-        HSVvector[1]+=1
-    if h>=26 and h<=40:
-        HSVvector[2]+=1
-    if h>=41 and h<=120:
-        HSVvector[3]+=1
-    if h>=121 and h<=190:
-        HSVvector[4]+=1
-    if h>=191 and h<=270:
-        HSVvector[5]+=1
-    if h>=271 and h<=295:
-        HSVvector[6]+=1
-    if h>=296 and h<=315:
-        HSVvector[7]+=1
-    s=Saturation(Pixel[0],Pixel[1],Pixel[2])
-    if s>=0 and s<=0.2:
-        HSVvector[8]+=1
-    if s>0.2 and s<=0.7:
-        HSVvector[9]+=1
-    if s>0.7 and s<=1:
-        HSVvector[10]+=1
-    v=Value(Pixel[0],Pixel[1],Pixel[2])
-    if v>=0 and v<=0.2:
-        HSVvector[11]+=1
-    if v>0.2 and v<=0.7:
-        HSVvector[12]+=1
-    if v>0.7 and v<=1:
-        HSVvector[13]+=1
+def get_result_image():
+    current_folder = os.path.dirname(os.path.abspath(__file__))
+    folder_name = 'archive\dataset'
+    folder_path = os.path.join(current_folder, folder_name)
+    dataset_paths = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file))]
+    input_path = os.path.join(current_folder, '18.jpg')
+    v1=Result(input_path,4)
 
+    similarities = [(similarity_value, os.path.basename(path)) for path, similarity_value in zip(dataset_paths, [cosineSimAvg(v1, Result(path,4)) * 100 for path in dataset_paths]) if similarity_value > 60]
+    similarities.sort(reverse=True, key=lambda x: x[0])
+    print(len(similarities))
+    print(similarities)
 
-def Compare(data1,data2):
-    HSVmatrix1=[[0 for i in range(14)] for i in range (9)]
+get_result_image()
 
-    rowCoord1=[0,RowSplit3(data1)[0],RowSplit3(data1)[1],Height(data1)]
-    colCoord1=[0,ColSplit3(data1)[0],ColSplit3(data1)[1],Width(data1)]
+t1_stop = process_time()
 
-    data1HSV=RGBtoHSV(data1)
-    nth1=0
-    for i in range (0,3):
-        for j in range (0,3):
-            print("image 1 is",formatPersen(nth1/9),"done")
-            for k in range (rowCoord1[i],rowCoord1[i+1]):
-                for l in range (colCoord1[j],colCoord1[j+1]):
-                    Quantification(HSVmatrix1[nth1],data1HSV[k][l])
-            nth1+=1
-    print("image 1 is done")
-    print("")
-
-    HSVmatrix2=[[0 for i in range(14)] for i in range (9)]
-
-    rowCoord2=[0,RowSplit3(data2)[0],RowSplit3(data2)[1],Height(data2)]
-    colCoord2=[0,ColSplit3(data2)[0],ColSplit3(data2)[1],Width(data2)]
-
-    data2HSV=RGBtoHSV(data2)
-    nth2=0
-    for i in range (0,3):
-        for j in range (0,3):
-            print("image 2 is",formatPersen(nth2/9),"done")
-            for k in range (rowCoord2[i],rowCoord2[i+1]):
-                for l in range (colCoord2[j],colCoord2[j+1]):
-                    Quantification(HSVmatrix2[nth2],data2HSV[k][l])
-            nth2+=1
-    print("image 2 is done")        
-    print("")
-            
-    CosineSimArr=[0 for i in range (9)]
-    for i in range (0,9):
-        CosineSimArr[i]=CosineSim(HSVmatrix1[i],HSVmatrix2[i])
-
-    hasil=Average(CosineSimArr,9)
-    return hasil
-
-def formatPersen(N):
-    N*=100
-    fN="{:.2f}".format(N)
-    sfN=str(fN)+"%"
-    return sfN
-
-def printHasil(data1,data2):
-    Hasil=Compare(data1,data2)
-    print("CosineSimilarity:", formatPersen(Hasil))
-
-printHasil(data1,data2)
+print("Elapsed time:", t1_stop-t1_start)
