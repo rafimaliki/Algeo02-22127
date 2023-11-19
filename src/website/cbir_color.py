756eeecbb2a7d7dff38522f016624c3a47232faa
rafi-sama <rafimaliki28@gmail.com>
2023-11-19 19:04:47 +0700

    fix struktur folder

---
+++ src/website/cbir_color.py
@@ -0,0 +1,130 @@
+from PIL import Image
+from numpy import asarray
+from time import process_time 
+import numpy as np
+import os
+
+# t1_start = process_time()  
+
+def RGBNorm(Pict):
+    Pict=Pict.astype(np.float32)
+    return (np.divide(Pict,255.0))
+
+def HSV(Pict):
+    
+    red_channel = Pict[:, :, 0]
+    green_channel = Pict[:, :, 1]
+    blue_channel = Pict[:, :, 2]
+    
+    cmax=np.max(Pict, axis=2)
+    cmin=np.min(Pict, axis=2)
+    delta=np.subtract(cmax,cmin)
+
+    boolmaxR = np.logical_and(delta!=0,cmax==red_channel)
+    boolmaxG = np.logical_and(delta!=0,cmax==green_channel)
+    boolmaxB = np.logical_and(delta!=0,cmax==blue_channel)
+    boolG = np.logical_and(delta==0,delta==0)
+    
+    htemp=np.zeros_like(red_channel,dtype=np.float32)
+    stemp=np.zeros_like(red_channel,dtype=np.float32)
+    vtemp=np.zeros_like(red_channel,dtype=np.float32)
+    
+    red_channel=RGBNorm(red_channel)
+    green_channel=RGBNorm(green_channel)
+    blue_channel=RGBNorm(green_channel)
+
+    cmax=RGBNorm(cmax)
+    delta=RGBNorm(delta)
+
+    htemp[boolG]=0
+    htemp[boolmaxR]=(60*(((green_channel[boolmaxR]-blue_channel[boolmaxR])/delta[boolmaxR])%6))
+    htemp[boolmaxG]=(60*(((blue_channel[boolmaxG]-red_channel[boolmaxG])/delta[boolmaxG])+2))
+    htemp[boolmaxB]=(60*(((red_channel[boolmaxB]-green_channel[boolmaxB])/delta[boolmaxB])+4))
+    
+    stemp[cmax==0]=0
+    stemp[cmax!=0]=delta[cmax!=0]/cmax[cmax!=0]
+
+    vtemp=cmax
+    
+    vector=Quantify(htemp,stemp,vtemp)
+    return vector
+
+def Quantify(hue,sat,val):
+    h=np.copy(hue)
+    s=np.copy(sat)
+    v=np.copy(val)
+
+    h[np.logical_and(h>315, h==0)]=0
+    h[np.logical_and(h>0, h<=25)]=1
+    h[np.logical_and(h>25, h<=40)]=2
+    h[np.logical_and(h>40, h<=120)]=3
+    h[np.logical_and(h>120, h<=190)]=4
+    h[np.logical_and(h>190, h<=270)]=5
+    h[np.logical_and(h>270, h<=295)]=6
+    h[np.logical_and(h>295, h<=315)]=7
+    
+    hueRange = [0,1,2,3,4,5,6,7]
+    huedict = {value: np.count_nonzero(h==value) for value in hueRange}
+    H = np.array(list(huedict.values()),dtype=np.uint32)
+
+    s[np.logical_and(s>0.7, s<=1)]=2
+    s[np.logical_and(s>0.2, s<=0.7)]=1
+    s[np.logical_and(s>=0, s<=0.2)]=0
+    
+    satRange = [0,1,2]
+    satdict = {value: np.count_nonzero(s==value) for value in satRange}
+    S = np.array(list(satdict.values()),dtype=np.uint32)
+
+    v[np.logical_and(v>0.7, v<=1)]=2
+    v[np.logical_and(v>0.2, v<=0.7)]=1
+    v[np.logical_and(v>=0, v<=0.2)]=0
+    
+    valRange = [0,1,2]
+    valdict = {value: np.count_nonzero(v==value) for value in valRange}
+    V = np.array(list(valdict.values()),dtype=np.uint32)
+
+    return np.concatenate((H,S,V))  
+
+def cosineSimAvg(v1,v2):
+    hasil=0.0
+    for i in range(16):
+        vDot=np.dot(v1[i],v2[i])
+        vNorm1=np.linalg.norm(v1[i])
+        vNorm2=np.linalg.norm(v2[i])
+        hasil+=np.divide(vDot,np.multiply(vNorm1,vNorm2))
+    return hasil/16
+
+def Result(Pict,n):
+    Pict=Image.open(Pict).convert("RGB")
+    Pict=asarray(Pict)
+    result_array = np.zeros((n*n,14), dtype=np.int64)
+    h=Pict.shape[0]//n
+    w=Pict.shape[1]//n
+    nth=0
+    for i in range (0,n):
+        for j in range (0,n):
+            result_array[nth]=HSV(Pict[i*h:h*(i+1)-1, j*w:w*(j+1)-1 ,:])
+            nth+=1
+    return result_array
+
+def get_result_images_color():
+    current_folder = os.path.dirname(os.path.abspath(__file__))
+    folder_name = 'static\dataset_picture'
+    input_name = 'static\submitted_picture'
+    input_path = os.path.join(current_folder, input_name)
+    folder_path = os.path.join(current_folder, folder_name)
+    dataset_paths = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file))]
+    input_path = os.path.join(input_path, 'submitted_image.png')
+
+    v1=Result(input_path,4)
+  
+    similarities = [[os.path.basename(path), similarity_value] for path, similarity_value in zip(dataset_paths, [cosineSimAvg(v1, Result(path,4)) * 100 for path in dataset_paths]) if similarity_value > 60]
+    similarities.sort(reverse=True, key=lambda x: x[1])
+
+    return similarities
+
+# get_result_image()
+
+# t1_stop = process_time()
+
+# print("Elapsed time:", t1_stop-t1_start)
---
+++ src/website/page_userinput.py
@@ -0,0 +1,172 @@
+import os, shutil, time
+from .cbir_texture import get_result_images_texture
+from .cbir_color import get_result_images_color
+from .webscrap import download_images
+from math import ceil
+
+from flask import Blueprint, render_template, request, redirect, url_for
+
+page_userinput = Blueprint('page_userinput', __name__)
+runtime = 0
+result_images = []
+selected_method = 0
+scroll_position = 0
+is_web_scrap = False
+
+@page_userinput.route('/userinput', methods=['GET', 'POST'])
+def home():
+
+    image_path = os.path.join('website/static/submitted_picture', 'submitted_image.png')
+
+    if request.method == 'POST':
+        if os.path.exists(image_path):
+            os.remove(image_path)
+
+    selected_image = 'submitted_image.png' if os.path.exists(image_path) else None
+    len_dataset = len(os.listdir('website/static/dataset_picture'))
+    if len_dataset:
+        dataset_images =  [image for image in os.listdir('website/static/dataset_picture') if image.endswith(('.png', '.jpg', '.jpeg'))]
+    else:
+        dataset_images = []
+
+    # Get len result images
+    len_result = len(result_images)
+
+    # Pagination
+    images_per_page = 5
+
+    total_pages_result = ceil(len(result_images) // images_per_page )
+    current_page_result = int(request.args.get('page_result', 1))
+    start_index_result = (current_page_result - 1) * images_per_page
+    end_index_result = start_index_result + images_per_page
+    paginated_images_result = result_images[start_index_result:end_index_result]
+
+    total_pages_dataset = ceil(len(dataset_images) // images_per_page )
+    current_page_dataset = int(request.args.get('page_dataset', 1))
+    start_index_dataset = (current_page_dataset - 1) * images_per_page
+    end_index_dataset = start_index_dataset + images_per_page
+    paginated_images_dataset = dataset_images[start_index_dataset:end_index_dataset]
+
+    return render_template("page_userinput.html", selected_image=selected_image, 
+                                                  len_dataset=len_dataset, 
+                                                  result_images=paginated_images_result, 
+                                                  total_pages_result=total_pages_result, 
+                                                  current_page_result=current_page_result, 
+                                                  runtime=runtime, 
+                                                  len_result=len_result,
+                                                  selected_method=selected_method,
+                                                  isWebScrap=is_web_scrap,
+                                                  total_pages_dataset=total_pages_dataset, 
+                                                  current_page_dataset=current_page_dataset, 
+                                                  dataset_images=paginated_images_dataset)
+
+@page_userinput.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'image' in request.files:
+        image = request.files['image']
+        image.save(os.path.join('website/static/submitted_picture', 'submitted_image.png'))
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/delete_image', methods=['GET','POST'])
+def delete_image():
+    image_path = os.path.join('website/static/submitted_picture', 'submitted_image.png')
+    if os.path.exists(image_path):
+        os.remove(image_path)
+    global selected_method
+    selected_method = 0
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/upload_dataset_images', methods=['POST'])
+def upload_dataset_images():
+    # Clear existing images in the database_picture folder
+    database_folder = 'website/static/dataset_picture'
+    if os.path.exists(database_folder):
+        shutil.rmtree(database_folder)
+    os.makedirs(database_folder)
+
+    # Save the uploaded images to the database_picture folder
+    if 'images[]' in request.files:
+        images = request.files.getlist('images[]')
+        for image in images:
+            image.save(os.path.join(database_folder, image.filename))
+
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/delete_all_dataset_images', methods=['GET','POST'])
+def delete_all_dataset_images():
+
+    global result_images
+    result_images = []
+    database_folder = 'website/static/dataset_picture'
+    if os.path.exists(database_folder):
+        shutil.rmtree(database_folder)
+        os.makedirs(database_folder)
+    global selected_method
+    selected_method = 0
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/cbir_color', methods=['POST'])
+def cbir_color():
+
+    global selected_method
+    selected_method = "color"
+
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/cbir_texture', methods=['POST'])
+def cbir_texture():
+
+    global selected_method
+    selected_method = "texture"
+
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/run_search', methods=['POST'])
+def run_search():
+    
+    global runtime
+    global result_images
+
+    if selected_method == "color":
+        runtime = float(time.time())
+
+        print("TRIGGER FUNCTION: CBIR Metode Color")
+        result_images = get_result_images_color() 
+        for image in result_images:
+            image[1] = round(image[1], 2)
+
+        runtime = f"{round(float(time.time()) - float(runtime),3)}"
+    
+    else:
+        runtime = float(time.time())
+
+        print("TRIGGER FUNCTION: CBIR Metode Tekstur")
+        result_images = get_result_images_texture()
+        for image in result_images:
+            image[1] = round(image[1], 2)
+
+        runtime = f"{round(float(time.time()) - float(runtime),3)}"
+    
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/toggle_webscrap_button', methods=['POST', 'GET'])
+def toggle_webscrap_button():
+
+    global is_web_scrap
+    if not(is_web_scrap):
+        is_web_scrap = True
+    else:
+        is_web_scrap = False
+    return redirect(url_for('page_userinput.home'))
+
+@page_userinput.route('/run_webscrap', methods=['POST', 'GET'])
+def run_webscrap():
+
+
+    link_input = request.form['link_input']
+    number_input = int(request.form['number_input'])
+    print("TRIGGER WEBSCRAP")
+
+    download_images(link_input, number_input)
+
+    return redirect(url_for('page_userinput.home'))
